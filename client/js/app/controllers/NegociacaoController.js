4a09618ad6e8890b03a7ef533ce96465b8ee8f65
anderood <anderood@hotmail.com>
2020-07-14 10:21:58 -0300

    Criado o Arquivo NegociacaoController.js

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,3 +1,27 @@
 class NegociacaoController {
-    
+
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        
+        event.preventDefault();
+        let data = new Date(...this._inputData
+            .value.split('-')
+            .map(function(item, indice){
+                if(indice == 1){
+                    return item - 1;
+                }
+                return item;
+            })
+
+        );
+        console.log(data)
+    }
 }
